9daf5576c0fce93994146da1c11f6b66de95aa1c
Developer Yusuf <developeryusuf@icloud.com>
2022-03-05 17:02:31 +0300

    gitignore

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,42 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_login_facebook/FlutterLoginFacebookPlugin.h>)
+#import <flutter_login_facebook/FlutterLoginFacebookPlugin.h>
+#else
+@import flutter_login_facebook;
+#endif
+
+#if __has_include(<google_sign_in/FLTGoogleSignInPlugin.h>)
+#import <google_sign_in/FLTGoogleSignInPlugin.h>
+#else
+@import google_sign_in;
+#endif
+
+#if __has_include(<sign_in_apple/SignInApplePlugin.h>)
+#import <sign_in_apple/SignInApplePlugin.h>
+#else
+@import sign_in_apple;
+#endif
+
+#if __has_include(<sign_in_with_apple/SignInWithApplePlugin.h>)
+#import <sign_in_with_apple/SignInWithApplePlugin.h>
+#else
+@import sign_in_with_apple;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterLoginFacebookPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterLoginFacebookPlugin"]];
+  [FLTGoogleSignInPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleSignInPlugin"]];
+  [SignInApplePlugin registerWithRegistrar:[registry registrarForPlugin:@"SignInApplePlugin"]];
+  [SignInWithApplePlugin registerWithRegistrar:[registry registrarForPlugin:@"SignInWithApplePlugin"]];
+}
+
+@end
